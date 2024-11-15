0b0948a07465af3a54ddbcf7961095ad2c49b81d
S.shiva krishna reddy <128881036+Shiva0krishna@users.noreply.github.com>
2024-11-15 23:01:18 +0530

    test

--- apps/httpprotocol/routes/Route1.js
+++ apps/httpprotocol/routes/Route1.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import  {callapi} from "./callapi.js";
+// import  {callapi} from "./callapi.js";
+import { getDatafromJson } from "./getDatafromJson.js";
 
 export const userRouter = Router();
 
@@ -9,13 +10,23 @@ userRouter.post('/chat', async(req, res) => {
     return res.status(400).json({ reply: 'Please enter a valid message.' });
   }
 
+  // try {
+  //   const response = await callapi(userMessage);
+  //   res.json({reply : response})  
+  //   console.log(`response is ${response}`)  
+  // } catch (error) {
+  //   res.status(500).json({reply : error})
+  //   console.log(`catch ${error}`)
+  // }
+
   try {
-    const response = await callapi(userMessage);
-    res.json({reply : response})  
-    console.log(`response is ${response}`)  
+    // temperoty data is fetched from data.json file instead of integrating external model.
+    const data = await getDatafromJson(userMessage);
+    res.json({reply : data})
+    console.log(`response is1 :${data}`);
   } catch (error) {
-    res.status(500).json({reply : error})
-    console.log(`catch ${error}`)
+    res.status(500).json({reply : "sever side problem,Try again later"})
+    console.log(error)
   }
 });
  
